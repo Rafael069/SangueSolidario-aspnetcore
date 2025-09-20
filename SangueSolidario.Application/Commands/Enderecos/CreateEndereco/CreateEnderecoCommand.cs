56c09353b99b93bbbcaa6e12386dd262750bea85
Rafael069 <santiagorafael069@gmail.com>
2025-09-20 20:49:10 -0300

    CQRS: Criação do Command Create da Entidade Endereco

---
+++ SangueSolidario.Application/Commands/Enderecos/CreateEndereco/CreateEnderecoCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SangueSolidario.Application.Commands.Enderecos.CreateEndereco
+{
+    public class CreateEnderecoCommand : IRequest<int>
+    {
+        public CreateEnderecoCommand(string cep, int id)
+        {
+            Cep = cep;
+            Id = id;
+        }
+        public string Cep { get; private set; }
+        public int Id { get; private set; }
+    }
+}
---
+++ SangueSolidario.Application/Commands/Enderecos/CreateEndereco/CreateEnderecoCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using SangueSolidario.Application.Commands.Doadores.CreateDoador;
+using SangueSolidario.Application.InputModels;
+using SangueSolidario.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SangueSolidario.Application.Commands.Enderecos.CreateEndereco
+{
+    public class CreateEnderecoCommandHandler : IRequestHandler<CreateEnderecoCommand, int>
+    {
+        private readonly SangueSolidarioDbContext _dbcontext;
+        private readonly HttpClient _httpClient;
+
+        public CreateEnderecoCommandHandler(SangueSolidarioDbContext dbcontext, HttpClient httpClient)
+        {
+            _dbcontext = dbcontext;
+            _httpClient = httpClient;
+        }
+
+        //Cria um novo endereço, agora integrando com ViaCepService
+        public async Task<int> Handle(CreateEnderecoCommand request, CancellationToken cancellationToken)
+        {
+            // Fazendo a requisição à API do ViaCEP
+           // var response = await _httpClient.GetAsync($"{request.Cep}/json/");
+           var response = await _httpClient.GetAsync($"https://viacep.com.br/ws/{request.Cep}/json/");
+
+            response.EnsureSuccessStatusCode();
+
+
+            // Deserializando a resposta da API do ViaCEP
+            var enderecoData = await response.Content.ReadAsStringAsync(cancellationToken);
+            var viaCepEndereco = JsonSerializer.Deserialize<NewEnderecoInputModel>(enderecoData);
+
+            // Criando a entidade Endereco
+            var endereco = new Endereco(
+                viaCepEndereco.Logradouro,
+                viaCepEndereco.Cidade,
+                viaCepEndereco.Estado,
+                viaCepEndereco.CEP,
+                   //viaCepEndereco.IdDoador = request.Id
+                request.Id // o doadorId que veio do command
+            );
+
+            // Salvando no banco
+            await _dbcontext.Enderecos.AddAsync(endereco, cancellationToken);
+            await _dbcontext.SaveChangesAsync(cancellationToken);
+
+            return endereco.Id;
+        }
+    }
+}
