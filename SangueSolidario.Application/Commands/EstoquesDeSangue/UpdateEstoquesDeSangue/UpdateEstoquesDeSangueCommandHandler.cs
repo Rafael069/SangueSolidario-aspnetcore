c9e52735b4700e0f34537938246f42900c394427
Rafael Santiago <santiagorafael069@gmail.com>
2025-10-01 20:58:57 -0300

    Update UpdateEstoquesDeSangueCommandHandler.cs

--- SangueSolidario.Application/Commands/EstoquesDeSangue/UpdateEstoquesDeSangue/UpdateEstoquesDeSangueCommandHandler.cs
+++ SangueSolidario.Application/Commands/EstoquesDeSangue/UpdateEstoquesDeSangue/UpdateEstoquesDeSangueCommandHandler.cs
@@ -24,24 +24,6 @@ namespace SangueSolidario.Application.Commands.EstoquesDeSangue.UpdateEstoquesDe
 
         public async Task<Unit> Handle(UpdateEstoquesDeSangueCommand request, CancellationToken cancellationToken)
         {
-            //var estoque = await _dbcontext.EstoquesSangue
-            //    .FirstOrDefaultAsync(e => e.TipoSanguineo == request.TipoSanguineo && e.FatorRh == request.FatorRh);
-
-            //if (estoque == null)
-            //{
-            //    // Se não existir, cria um novo registro no estoque
-            //    estoque = new EstoqueSangue(doador.TipoSanguineo, doador.FatorRh, quantidadeML);
-            //   await _dbcontext.EstoquesSangue.Add(estoque);
-            //}
-            //else
-            //{
-            //    // Se já existir, soma a quantidade doada
-            //    estoque.QuantidadeML += quantidadeML;
-            //    await _dbcontext.EstoquesSangue.Update(estoque);
-            //}
-
-            //_dbcontext.SaveChanges();
-
 
             var estoque = await _dbcontext.EstoquesSangue
                 .FirstOrDefaultAsync(e =>
